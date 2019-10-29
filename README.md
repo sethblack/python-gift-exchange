406a5c16697b66f1122b840a0beee6c77bd63552
Seth Black <sblack@sethserver.com>
2019-10-29 06:48:07 -0500

    Create Person class.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # knn-gift-exchange
+
 Holiday Gift Exchange Picker Using KNN
+
+Cities database provided by (https://simplemaps.com/data/us-cities)[https://simplemaps.com/data/us-cities]
---
+++ knnge/person.py
@@ -0,0 +1,45 @@
+import hashlib
+
+from datetime import datetime
+from citydb import lookup_city
+
+class Person():
+    def __init__(self, name, dob, gender, city, exchange_history):
+        self.name = name
+        self.dob = datetime.strptime(dob, '%m/%d/%Y')
+        self.gender = gender
+        self.lat_lng = lookup_city(city)
+        self.exchange_history = exchange_history
+
+    def vector(self):
+        age_point = self.age_in_days / 43830.
+        gender_point = 0. if self.gender == 'M' else 1.
+
+        return (age_point, gender_point, self.lat_lng[0], self.lat_lng[1])
+
+    @property
+    def age_in_days(self):
+        return (datetime.now() - self.dob).days
+
+    def euclidean_distance(self, other):
+        x_vector = self.vector()
+        y_vector = other.vector()
+
+        return sum([(x - y) ** 2 for x, y in zip(x_vector, y_vector)]) ** .5
+
+    def l1_distance(self, other):
+        x_vector = self.vector()
+        y_vector = other.vector()
+
+        return sum([abs(x - y) for x, y in zip(x_vector, y_vector)])
+
+    def __str__(self):
+        return f'{self.name}: {self.__hash__()}'
+
+    def __hash__(self):
+        return int(hashlib.md5(
+            f'{self.name}:{self.dob}:{self.gender}:{self.lat_lng}'.encode('utf-8')
+        ).hexdigest()[:8], 16)
+
+    def __eq__(self, other):
+        return type(self) == type(other) and self.__hash__() == other.__hash__()
